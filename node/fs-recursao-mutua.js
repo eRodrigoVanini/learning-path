b71e586efa56c6619f20e2ecb857faa3e1e35628
eRodrigoVanini <arch.rvanini@gmail.com>
2026-02-06 09:21:50 -0300

    Add isDirectory function

--- node/fs-recursao-mutua.js
+++ node/fs-recursao-mutua.js
@@ -1,18 +1,37 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-//File System para varrer as pastas;
-//.then porque é uma promise;
-//.catch para tratar caso erro.
+//==============File System para varrer as pastas;
+//==========.then porque é uma promise;
+//====.catch para tratar caso erro.
 
-//Sem o path ele leria a pasta 'errada'. Ele retona o conteúdo da pasta 'pai' learning-path, enquanto eu quero o conteúdo da minha pasta 'node'.
+//==========Sem o path ele leria a pasta 'errada'. Ele retona o conteúdo da pasta 'pai' learning-path, enquanto eu quero o conteúdo da minha pasta 'node'.
 /*
-fs.readdir("./")
+fs.readdir("./") 
   .then((files) => console.log(files))
   .catch((e) => console.log(e));
 */
 
-//Preciso passar o caminho ABSOLUTO com path
-fs.readdir(path.resolve(__dirname))
+//==========Precisaria passar o caminho ABSOLUTO com path
+/*fs.readdir(path.resolve(__dirname))
   .then((files) => console.log(files))
   .catch((e) => console.log(e));
+*/
+
+//========= Vou otimizar criando uma função
+//====Uma alternativa ao then/catch é o async await
+async function readdir(rootDir) {
+  rootDir = rootDir || path.resolve(__dirname);
+  const files = await fs.readdir(rootDir);
+  walk(files, rootDir);
+}
+
+async function walk(files, rootDir) {
+  for (let file of files) {
+    const fileFullPath = path.resolve(rootDir, file);
+    const stats = await fs.stat(fileFullPath);
+    console.log(file, stats.isDirectory());
+  }
+}
+
+readdir("/Users/arqrv/Documents/00-dev/projetos");
